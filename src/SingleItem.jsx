77aaeeac4224ac7fec65d9b219383edf03eefde5
komu <sanjeevniraj2001@gmail.com>
2023-10-18 20:22:50 +0530

    initial commit

---
+++ src/SingleItem.jsx
@@ -0,0 +1,28 @@
+const SingleItem = ({ name, id, completed, removeItem, editItem }) => {
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => editItem(id)}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: completed ? 'line-through' : 'none',
+        }}
+      >
+        {name}
+      </p>
+      <button
+        type="button"
+        className="btn remove-btn"
+        onClick={() => removeItem(id)}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+
+export default SingleItem;
